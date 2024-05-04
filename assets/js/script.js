e085e2e4ee9b38f63087e4f43507b06276037945
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-05-04 21:00:28 +0000

    Create a DOM variables in Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// DOM variables
+const choices = ["rock", "paper", "scissors"];
+const messages = document.getElementById("messages");
+const buttons = document.getElementsByName("check");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.document.getElementById("computer-score");
+const playerImage = document.document.getElementById("player-image");
+const computerImage = document.document.getElementById("computer-image");
