34aeae7114b0f4cfee9796e62e98667d08c0b9b4
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-05-04 21:27:10 +0000

    Add event listeners and the main game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,22 @@ const playerScore = document.getElementById("player-score");
 const computerScore = document.document.getElementById("computer-score");
 const playerImage = document.document.getElementById("player-image");
 const computerImage = document.document.getElementById("computer-image");
+
+// Get the button elements and add event listeners 
+
+for (let button of buttons ) {
+    button.addEventListener("click", function() {
+        letPlayerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    } );
+}
+
+// The main game function
+
+function playGame(playerChoice) {
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let result = checkWinner(choices [computerchoice], choices[playerChoice]);
+    updateScores(result);
+}
